e2f22c72ed874e046972750eb8eca352ddf097bd
simontegg <simon@enspiral.com>
2015-08-06 16:46:39 +1200

    tweak api, and rename

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 var m = require('matrix-utilities')
 
 //[x, y, scale], [x, y, scalar]
-module.exports = function (inVector, scalar) {
+module.exports = function (coords, transform) {
   var ma0 = m.Identity()
   var ma1 = m.Identity()
   var ma2
-  var ma3
+  var transformMatrix
   var outMatrix
 
-  ma0[0][3] = inVector[0]
-  ma0[1][3] = inVector[1]
-  ma0[0][0] = inVector[2]
-  ma0[1][1] = inVector[2]
+  ma0[0][3] = coords.vector[0] //x
+  ma0[1][3] = coords.vector[1] //y
+  ma0[0][0] = coords.scale //k
+  ma0[1][1] = coords.scale //k
 
-  ma1[0][3] = scalar[0]
-  ma1[1][3] = scalar[1]
+  ma1[0][3] = transform.vector[0] //x
+  ma1[1][3] = transform.vector[1] //y
 
-  if (scalar[2] !== 1) { //zoom to/from a point scalar[0], scalar[1]
-    ma1[0][0] = scalar[2]
-    ma1[1][1] = scalar[2]
+  if (transform.scale && transform.scale !== 1) { //zoom to/from a point scalar[0], scalar[1]
+    ma1[0][0] = transform.scale
+    ma1[1][1] = transform.scale
 
     ma2 = m.Identity()
-    ma2[0][3] = -scalar[0]
-    ma2[1][3] = -scalar[1]
+    ma2[0][3] = -transform.vector[0]
+    ma2[1][3] = -transform.vector[1]
 
-    ma3 = m.multiply(ma0, ma1)
-    outMatrix = m.multiply(ma3, ma2)
+    transformMatrix = m.multiply(ma1, ma2)
+    outMatrix = m.multiply(ma0, transformMatrix)
   } else { //drag to a point scalar[0], scalar[1]
     outMatrix = m.multiply(ma0, ma1)
   }
 
-  return [outMatrix[0][3], outMatrix[1][3], outMatrix[0][3]]
+  return { vector: [ outMatrix[0][3], outMatrix[1][3] ], scale: outMatrix[0][0] }
 }
