55481ac5dfa03ad293733a5e58c3ab9489648978
simontegg <simon@enspiral.com>
2015-08-07 16:52:26 +1200

    BREAKING CHANGE to api
    now takes in:
    1. current [x, y, scale]
    2. transform [x, y, scale]
    
    returns [x, y, scale]

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
 var m = require('matrix-utilities')
 
-//[x, y, scale], [x, y, scalar]
-module.exports = function (coords, transform) {
+module.exports = zoom
+
+// input
+//   current: [x, y, scale]
+//   transform: [x, y, scale]
+// output: [x, y, scale]
+function zoom (current, transform) {
   var ma0 = m.Identity()
   var ma1 = m.Identity()
   var ma2 = m.Identity()
   var transformMatrix
   var outMatrix
 
-  ma0[0][3] = coords.vector[0] //x
-  ma0[1][3] = coords.vector[1] //y
-  ma0[0][0] = coords.scale //k
-  ma0[1][1] = coords.scale //k
+  ma0[0][3] = current[0] //x
+  ma0[1][3] = current[1] //y
+  ma0[0][0] = current[2] //k
+  ma0[1][1] = current[2] //k
 
-  ma1[0][3] = transform.vector[0] //x
-  ma1[1][3] = transform.vector[1] //y
-  ma1[0][0] = transform.scale
-  ma1[1][1] = transform.scale
+  ma1[0][3] = transform[0] //x
+  ma1[1][3] = transform[1] //y
+  ma1[0][0] = transform[2]
+  ma1[1][1] = transform[2]
 
-  ma2[0][3] = -transform.vector[0]
-  ma2[1][3] = -transform.vector[1]
+  ma2[0][3] = -transform[0]
+  ma2[1][3] = -transform[1]
 
   transformMatrix = m.multiply(ma1, ma2)
   outMatrix = m.multiply(ma0, transformMatrix)
 
-  return { vector: [ outMatrix[0][3], outMatrix[1][3] ], scale: outMatrix[0][0] }
+  return [ outMatrix[0][3], outMatrix[1][3], outMatrix[0][0] ]
 }
