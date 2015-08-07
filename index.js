1e3a77f4a622e05bf1c740cb0449e38067faf86b
simontegg <simon@enspiral.com>
2015-08-07 13:54:19 +1200

    rm unused case

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var m = require('matrix-utilities')
 module.exports = function (coords, transform) {
   var ma0 = m.Identity()
   var ma1 = m.Identity()
-  var ma2
+  var ma2 = m.Identity()
   var transformMatrix
   var outMatrix
 
@@ -15,20 +15,14 @@ module.exports = function (coords, transform) {
 
   ma1[0][3] = transform.vector[0] //x
   ma1[1][3] = transform.vector[1] //y
+  ma1[0][0] = transform.scale
+  ma1[1][1] = transform.scale
 
-  if (transform.scale && transform.scale !== 1) { //zoom to/from a point scalar[0], scalar[1]
-    ma1[0][0] = transform.scale
-    ma1[1][1] = transform.scale
+  ma2[0][3] = -transform.vector[0]
+  ma2[1][3] = -transform.vector[1]
 
-    ma2 = m.Identity()
-    ma2[0][3] = -transform.vector[0]
-    ma2[1][3] = -transform.vector[1]
-
-    transformMatrix = m.multiply(ma1, ma2)
-    outMatrix = m.multiply(ma0, transformMatrix)
-  } else { //drag to a point scalar[0], scalar[1]
-    outMatrix = m.multiply(ma0, ma1)
-  }
+  transformMatrix = m.multiply(ma1, ma2)
+  outMatrix = m.multiply(ma0, transformMatrix)
 
   return { vector: [ outMatrix[0][3], outMatrix[1][3] ], scale: outMatrix[0][0] }
 }
