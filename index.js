e7b8d7e466997dcf9710a602aaafac947a96228c
simontegg <simon@enspiral.com>
2015-08-07 17:14:32 +1200

    failed attempt to port to using gl-mat4

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-var m = require('matrix-utilities')
+var create = require('gl-mat4/create')
+var identity = require('gl-mat4/identity')
+var multiply = require('gl-mat4/multiply')
 
 module.exports = zoom
 
@@ -7,27 +9,32 @@ module.exports = zoom
 //   transform: [x, y, scale]
 // output: [x, y, scale]
 function zoom (current, transform) {
-  var ma0 = m.Identity()
-  var ma1 = m.Identity()
-  var ma2 = m.Identity()
-  var transformMatrix
-  var outMatrix
+  var ma0 = create()
+  var ma1 = create()
+  var ma2 = create()
 
-  ma0[0][3] = current[0] //x
-  ma0[1][3] = current[1] //y
-  ma0[0][0] = current[2] //k
-  ma0[1][1] = current[2] //k
+  identity(ma0)
+  identity(ma1)
+  identity(ma2)
 
-  ma1[0][3] = transform[0] //x
-  ma1[1][3] = transform[1] //y
-  ma1[0][0] = transform[2]
-  ma1[1][1] = transform[2]
+  var transformMatrix = create()
+  var outMatrix = create()
 
-  ma2[0][3] = -transform[0]
-  ma2[1][3] = -transform[1]
+  ma0[0*4+3] = current[0] //x
+  ma0[1*4+3] = current[1] //y
+  ma0[0*4+0] = current[2] //k
+  ma0[1*4+1] = current[2] //k
 
-  transformMatrix = m.multiply(ma1, ma2)
-  outMatrix = m.multiply(ma0, transformMatrix)
+  ma1[0*4+3] = transform[0] //x
+  ma1[1*4+3] = transform[1] //y
+  ma1[0*4+0] = transform[2]
+  ma1[1*4+1] = transform[2]
 
-  return [ outMatrix[0][3], outMatrix[1][3], outMatrix[0][0] ]
+  ma2[0*4+3] = -transform[0]
+  ma2[1*4+3] = -transform[1]
+
+  multiply(transformMatrix, ma1, ma2)
+  multiply(outMatrix, ma0, transformMatrix)
+
+  return [ outMatrix[0*4+3], outMatrix[1*4+3], outMatrix[0*4+0] ]
 }
